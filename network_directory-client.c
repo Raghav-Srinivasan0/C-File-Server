c78ad00ac88ebee26887e68ab4b771104eddb849
RaghavSrinivasan0 <raghavsrinivasan0@gmail.com>
2023-12-02 23:17:38 -0500

    update client

--- network_directory-client.c
+++ network_directory-client.c
@@ -1,23 +1,26 @@
 #include "network_directory.h"
 
-int main()
+int main(char **argv)
 {
-    char* resultDir = "result_dir/";
-    while(true)
+    char *resultDir = "result_dir/";
+    while (true)
     {
         char filename[30];
-        scanf("%30s",filename);
-        if (strcmp("exit_client",filename) == 0) break;
-        char* res = client_request("tcp://localhost:80",filename);
-        if (strcmp("EXIT",filename) == 0) break;
-        if (strcmp(res,UNABLE) == 0) continue;
-        char* fullfilename = calloc(30+strlen(resultDir)+1,sizeof(char));
-        memcpy(fullfilename,resultDir,strlen(resultDir)+1);
-        strcat(fullfilename,filename);
+        scanf("%30s", filename);
+        if (strcmp("exit_client", filename) == 0)
+            break;
+        char *res = client_request(argv[1], filename);
+        if (strcmp("EXIT", filename) == 0)
+            break;
+        if (strcmp(res, UNABLE) == 0)
+            continue;
+        char *fullfilename = calloc(30 + strlen(resultDir) + 1, sizeof(char));
+        memcpy(fullfilename, resultDir, strlen(resultDir) + 1);
+        strcat(fullfilename, filename);
         remove(fullfilename);
-        printf("Opening %s ...\n",fullfilename);
-        FILE* f = fopen(fullfilename,"a");
-        fputs(res,f);
+        printf("Opening %s ...\n", fullfilename);
+        FILE *f = fopen(fullfilename, "a");
+        fputs(res, f);
         fclose(f);
         free(fullfilename);
     }
